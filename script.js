35cfafef12b75ef34d00a169e9e0a4cff2d64585
Matheus <matheusbairromaua@gmail.com>
2025-02-18 22:11:14 -0300

    att script.js

--- script.js
+++ script.js
@@ -16,6 +16,6 @@ function toggleMode() {
     )
   } else {
     img.setAttribute("src", "./assets/profilepurple.PNG")
-    img.setAttribute("alt", "Foto de Matheus Dev sorrindo, usando camisa branca, barba curta e fundo roxo.")
+    img.setAttribute("alt", "Foto de Matheus Dev sério, de óculos escuro com a lente azul. Olhando para o lado e segurando a armação do óculos com uma de suas mãos, usando casaco preto, sem barba e fundo roxo.")
   }
 }
