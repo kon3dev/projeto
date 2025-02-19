3255dfad1ea57272cb784f86c70bda42d65da00b
Matheus <matheusbairromaua@gmail.com>
2025-02-18 21:40:24 -0300

    test

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/profilepurple.PNG")
-  } else {
     img.setAttribute("src", "./assets/profileyellow.PNG")
+  } else {
+    img.setAttribute("src", "./assets/profilepurple.PNG")
   }
 }
